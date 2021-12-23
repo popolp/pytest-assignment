41e472fe43764d3bff9702f6441fabe800ee24b6
popolp <omerpopol@gmail.com>
2021-12-23 23:42:25 +0200

    deleted classes directory

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,3 @@
 import sys
 # adding classes to the system path
-sys.path.insert(0, 'C:/Users/omerp/Documents/pytest-assignment/src/classes')
+sys.path.insert(0, 'C:/Users/omerp/Documents/pytest-assignment/src')
